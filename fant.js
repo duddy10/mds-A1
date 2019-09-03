e33e6d57fb36040d0d81881273e7a487a3975180
duddy <theo_u@hotmail.com>
2019-09-03 18:32:54 +0200

    added an element generator for ads

--- fant.js
+++ fant.js
@@ -1,6 +1,7 @@
 let users = [];
 let ads = [];
 let currentUser = null;
+let adsPrinted = 0;
 // let imageInput = document.getElementById("uploadImages")
 // imageInput.addEventListener("change", function (e) {
 //   console.log(imageInput.files)
@@ -17,7 +18,12 @@ let currentUser = null;
 function toggleView(currentView, nextView){
   document.getElementById(currentView).style.display = "none";
   document.getElementById(nextView).style.display = "block";
+
+  if(nextView === "ads"){
+      adsLoader();
+    }
 }
+
 
 // retrieves the input of create user page and creates a user object
 // Then stores this user in an array and toggles to the ads view
@@ -91,6 +97,51 @@ function updateHeader(){
   document.getElementById("me").innerHTML = currentUser.username;
 }
 
+// creates elements for ads
+function adsLoader(){
+
+  // gets the ads wrapper
+  let wrapper = document.getElementById("ads");
+
+  // chekcs if another ad has been added
+  if (adsPrinted != ads.length){
+
+    // only loops through unprinted ads
+    for(let i = ads.length-1; i >= adsPrinted-1; i--){
+
+      // initialises an ads "box"
+      let box = document.createElement("div");
+      box.id = i.toString();
+      box.className = "box";
+
+      // initialise an ads content
+      let img = document.createElement("img");
+      img.src = ads[i].images[0];
+      box.appendChild(img);
+
+      let title = document.createElement("h1");
+      title.innerHTML = ads[i].title;
+      box.appendChild(title);
+
+      let price = document.createElement("p");
+      price.innerHTML = ads[i].price + ",-";
+      box.appendChild(price);
+
+      let description = document.createElement("p");
+      description.innerHTML = ads[i].description;
+      box.appendChild(description);
+
+      // inserts the ads on top (we want newest ads first)
+      wrapper.insertBefore(box, wrapper.childNodes[0]);
+    }
+  }
+
+}
+
+function updateAds(){
+
+}
+
 // user class
 class User {
   constructor(firstname, lastname, streetAdress, city, postalCode, emailAdress, username, password) {
@@ -113,15 +164,10 @@ class Ad {
     this.description = description;
     this.images = images;
   }
-
-  createElement () {
-    return `
-      <div>
-        <h1>${this.title}</h1>
-       </div>
-    `
-  }
 }
 
 // initialises a user
 users.push(new User("theodor", "urtegård", "nedre strandgate 9d", "ålesund", "6004", "theo_u@hotmail.com", "theodor","theodor"));
+ads.push(new Ad("Logik tv", "500", "Selger ein brukt logik TV, er blitt nokre år gammal. 29 tommer HD LED TV. Kjøper betaler frakt. Ny eller brukt:	Brukt", ["pictures/fant1.jpg"]));
+ads.push(new Ad("Siemens Platetopp 50%", "7 900", "Selger induksjonsplatetopp som har stått utstillt i vår butikk på Lade. 80cm. Veil pris: 15 799,- INGEN reservasjon, 100% forskuddsbetaling. MÅ hentes på Kvik lade i uke 49/50. 2 års garanti fra kjøpsdato. Ingen returrett. Spesifikasjoner: https://www.siemens-home.bsh-group.com/no/produktliste/ED877FSF1X Med forbehold om feil i annonsetekst.", ["pictures/fant2.jpg"]));
+ads.push(new Ad("PC-veske", "250", "Selger en meget fin og praktisk skulderveske til bærbar PC. Den er av merket LONDON FOG (Sterling Edition) Som det kan sees av bildet, har den mange oppbevaringslommer. selges til kr 250,- eller høystbydende", ["pictures/fant3.jpg"]));
